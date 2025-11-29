0604b768494e2b66a49c16f5406d2e7734226d29
Adrian <adriankolodziej909@gmail.com>
2025-11-29 21:25:46 +0100

    Add messages page plus modify styles

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import '../Styles/Signup.css'
+import styles from '../Styles/Signup.module.css'
 
 function Signup() {
   return (
-    <div className='container'>
-        <div className="signup">
-            <form className="signup-form">
-                <h1>Sign Up</h1>
-                <div className='form-grid'>
-                    <label htmlFor='email'>E-mail</label>
-                    <input type="email" placeholder='johndoe@gmail.com' name='email'></input>
-                    <label htmlFor='password'>Password</label>
-                    <input type="password" placeholder='********' name='password'></input>
-                    <button className='signup-btn'>Sign Up</button>
-                    <button className='login-btn'>Login</button>   
-                </div>
-            </form>
-        </div>
-        <div className="info">
-            <h3>Simple messaging thing made by a simpleton</h3><br></br>
-            <p>
-                Yo so this is a very basic messaging app, prolly 
-                quite trash and ineficient so dont judge</p>
+    <div className={styles.container}>
+        <div className={styles.signup_card}>
+            <div className={styles.signup}>
+                <form className={styles.signup_form}>
+                    <h1>Sign Up</h1>
+                    <div className={styles.form_grid}>
+                        <label htmlFor='email'>E-mail</label>
+                        <input type='email' placeholder='johndoe@gmail.com' name='email'></input>
+                        <label htmlFor={styles.password}>Password</label>
+                        <input type='password' placeholder='********' name='password'></input>
+                        <button className={styles.signup_btn}>Sign Up</button>
+                        <button className={styles.login_btn}>Login</button>   
+                    </div>
+                </form>
+            </div>
+            <div className={styles.info}>
+                <h3>Simple messaging thing made by a simpleton</h3><br></br>
+                <p>
+                    Yo so this is a very basic messaging app, prolly 
+                    quite trash and ineficient so dont judge</p>
+            </div>
         </div>
     </div>
   )
--- src/Pages/Messages.jsx
+++ src/Pages/Messages.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../Styles/Messages.module.css'
 
 function Messages() {
+  const [username, setUsername] = useState("Username")
+  const [content, setContent] = useState("Content")
   return (
     <>
-        <h1>Messages</h1>
+      <header className={styles.header}>Messages</header>
+      <div className={styles.messages_container}>
+        <div className={styles.messages}>
+
+          {/* MESSAGE STRUCTURE BELOW*/}
+
+          <div className={styles.message} id='message-1'>
+            <div className={styles.message_left}>
+              <div className={styles.message_pfp}></div>
+            </div>
+            <div className={styles.message_right}>
+              <div className={styles.message_username}>{username}</div>
+              <div className={styles.message_content}>{content}</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className={styles.user_message}>
+          <form>
+            <input type='text' placeholder='Write a message!'></input>
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
